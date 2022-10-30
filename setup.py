dc23cb247001e50b79cdc70cd58749c6b2f52fdb
EnDa Company <102385844+EnDaTeam@users.noreply.github.com>
2022-10-30 13:41:08 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='CAPP',
-    version='1.0',
-    author="EnDaTeam",
-    packages=find_packages('CAPP'),
-    package_dir={'': 'CAPP'},
-    url='https://github.com/EnDaTeam/EnDa-Console-App-Package',
-    keywords='console',
-    install_requires=[
-          'colorama>=0.4.6',
-          'pystyle>=2.0',
-          'pyfiglet>=0.8.0'
-      ],
-
-)
+setup()
